d05de17d2324723d6d0f3d65f30b47f123aec7e9
Marius Arlauskas <marius.arlauskas01.dev@gmail.com>
2024-08-22 20:34:03 +0300

    add tests

--- src/tests/test_driver.py
+++ src/tests/test_driver.py
@@ -1,9 +1,9 @@
+from pathlib import Path
+
+import pytest
 from nodriver import Browser
 
 from src.scraping_service.helpers.driver import DriverClient
-import pytest
-
-from pathlib import Path
 
 TEST_PATH = Path(__file__).parent
 TEST_DATA_PATH = TEST_PATH / "test_data"
@@ -15,17 +15,32 @@ TEST_FILENAMES = [
     "search_Python.html",
     "search_Rust.html",
 ]
+FILE_PATHS = [TEST_DATA_PATH / filename for filename in TEST_FILENAMES]
 
 
-# function to load all the test files
 @pytest.fixture
 def htmls():
-    file_paths = [TEST_DATA_PATH / filename for filename in TEST_FILENAMES]
-    return [file_path.read_text() for file_path in file_paths]
+    return [file_path.read_text() for file_path in FILE_PATHS]
 
 
-async def test_initialize_driver():
-    timed_driver = DriverClient()
-    await timed_driver.initialize()
-    assert isinstance(timed_driver.browser, Browser)
+@pytest.fixture
+async def driver():
+    driver = DriverClient()
+    await driver.initialize()
+    yield driver
+    await driver.close()
 
+
+@pytest.mark.asyncio
+async def test_initialize_driver(driver):
+    assert isinstance(driver.browser, Browser)
+
+
+@pytest.mark.asyncio
+async def test_get_html(driver, htmls):
+    for original_html, path in zip(htmls, FILE_PATHS):
+        # use file htmls to fetch through driver
+        fetched_html = await driver.get_html("file://" + str(path))
+        assert (
+            fetched_html[:1000] == original_html[:1000]
+        )  # Not comparing the whole due to dynamic content
