8720a20d24570381c306445d0c3d98ac1f49afad
Marius Arlauskas <marius.arlauskas01.dev@gmail.com.com>
2025-08-04 20:58:15 +0300

    add restarts

--- scraping_service/helpers/lifespan.py
+++ scraping_service/helpers/lifespan.py
@@ -1,23 +1,40 @@
+import asyncio
 import logging
 from collections.abc import AsyncGenerator
-from contextlib import asynccontextmanager
+from contextlib import asynccontextmanager, suppress
 
 from fastapi import FastAPI
 
 from scraping_service.helpers.driver import DriverClient
 
 LOGGER = logging.getLogger(__name__)
+RESTART_TIMER = 30 * 60  # 30 minutes in seconds
+
+
+async def restart_driver_periodically(app: FastAPI) -> None:
+    while True:
+        LOGGER.info("Restarting driver client...")
+        try:
+            await app.driver_client.close()
+            LOGGER.info("Old driver client closed.")
+        except Exception:
+            LOGGER.exception("Error closing driver client")
+        app.driver_client = DriverClient()
+        await app.driver_client.initialize()
+        LOGGER.info("New driver client initialized.")
+        await asyncio.sleep(30 * 60)  # wait 30 minutes
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+async def lifespan(app: FastAPI) -> AsyncGenerator[None]:
     """Manage resources during the lifespan of the application."""
-    # Set up
-    LOGGER.info("Setting up timed driver.")
-    app.driver_client = DriverClient()
-    await app.driver_client.initialize()
-    LOGGER.info("Driver setup complete.")
-    yield
-    # Clean up
-    app.driver_client.close()
-    LOGGER.info("Driver quit.")
+    task = asyncio.create_task(restart_driver_periodically())
+    try:
+        yield
+    finally:
+        # Cancel the background task on shutdown
+        task.cancel()
+        with suppress(asyncio.CancelledError):
+            await task
+        app.driver_client.close()
+        LOGGER.info("Driver quit.")
